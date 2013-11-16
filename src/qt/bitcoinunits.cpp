b287bf9d9d2c0fc0e9cc79530c6b270334ab30a0
Beniu <beniu@beniu-VirtualBox.(none)>
2013-11-16 11:25:17 +0000

    changes

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("FOO");
-    case mBTC: return QString("mFOO");
-    case uBTC: return QString::fromUtf8("μFOO");
+    case BTC: return QString("GAM");
+    case mBTC: return QString("mGAM");
+    case uBTC: return QString::fromUtf8("μGAM");
     default: return QString("???");
     }
 }
@@ -45,13 +45,13 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("FooCoin");
-    case mBTC: return QString("milliFooCoin (1 / 1,000)");
-    case uBTC: return QString("microFooCoin (1 / 1,000,000)");
+    case BTC: return QString("GamCoin");
+    case mBTC: return QString("milliGamCoin (1 / 1,000)");
+    case uBTC: return QString("microGamCoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
-//a single unit (.00000001) of FooCoin is called a "wander."
+//a single unit (.00000001) of GamCoin is called a "wander."
 qint64 BitcoinUnits::factor(int unit)
 {
     switch(unit)
